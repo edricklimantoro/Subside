a26b5da114b07e503e3e7b9a4fd8006b65d384e7
Bintang Anandhiya <bintang.anandhiya@my.sampoernauniversity.ac.id>
2022-11-17 05:54:20 +0700

    Add CardView Implementation on Gradle, Create User ProfileDisplay

---
+++ app/src/main/java/com/example/subside/ProfileDisplay.java
@@ -0,0 +1,12 @@
+package com.example.subside;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfileDisplay extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_display);
+    }
+}
